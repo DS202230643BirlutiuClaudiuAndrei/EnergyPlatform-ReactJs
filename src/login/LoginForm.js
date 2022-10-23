d30ae49e15127e1551f7d5010fcfa1e29e124f6d
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-10-23 12:00:22 +0300

    added login form

---
+++ src/login/LoginForm.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from "react";
+import { Col, Row } from "reactstrap";
+import { FormGroup, Input, Label } from "reactstrap";
+import Button from "react-bootstrap/Button";
+
+import Validate from "./validators/Validator.js";
+import APIResponseErrorMessage from "../commons/errorhandling/api-response-error-message";
+
+const formControlsInit = {
+  email: {
+    value: "",
+    placeholder: "Email...",
+    valid: false,
+    touched: false,
+    validationRules: {
+      emailValidator: true,
+      isRequired: true,
+    },
+  },
+  password: {
+    value: "",
+    placeholder: "Password...",
+    valid: false,
+    touched: false,
+    validationRules: {
+      isRequired: true,
+      minLength: 5,
+    },
+  },
+};
+
+function LoginForm(props) {
+  const [error, setError] = useState({ status: 0, errorMessage: null });
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [formControls, setFormControls] = useState(formControlsInit);
+
+  function handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    let updatedControls = { ...formControls };
+
+    let updatedFormElement = updatedControls[name];
+
+    updatedFormElement.value = value;
+    updatedFormElement.touched = true;
+    updatedFormElement.valid = Validate(
+      value,
+      updatedFormElement.validationRules
+    );
+    updatedControls[name] = updatedFormElement;
+
+    let formIsValid = true;
+    for (let updatedFormElementName in updatedControls) {
+      formIsValid =
+        updatedControls[updatedFormElementName].valid && formIsValid;
+    }
+
+    setFormControls((formControls) => updatedControls);
+    setFormIsValid((formIsValidPrev) => formIsValid);
+  }
+
+  function registerPerson(person) {
+    /*return API_USERS.postPerson(person, (result, status, err) => {
+      if (result !== null && (status === 200 || status === 201)) {
+        console.log("Successfully inserted person with id: " + result);
+        props.reloadHandler();
+      } else {
+        setError((error) => ({ status: status, errorMessage: err }));
+      }
+    });*/
+  }
+
+  function handleSubmit() {
+    let person = {
+      name: formControls.name.value,
+      email: formControls.email.value,
+      age: formControls.age.value,
+      address: formControls.address.value,
+    };
+    registerPerson(person);
+  }
+
+  return (
+    <div>
+      <FormGroup id="email">
+        <Label for="emailField"> Email: </Label>
+        <Input
+          name="email"
+          id="emailField"
+          placeholder={formControls.email.placeholder}
+          onChange={handleChange}
+          defaultValue={formControls.email.value}
+          touched={formControls.email.touched ? 1 : 0}
+          valid={formControls.email.valid}
+          required
+        />
+        {formControls.email.touched && !formControls.email.valid && (
+          <div className={"error-message"}>
+            {" "}
+            * Email must have a valid format
+          </div>
+        )}
+      </FormGroup>
+
+      <FormGroup id="password">
+        <Label for="passwordField"> Password: </Label>
+        <Input
+          name="password"
+          id="passwordField"
+          type="password"
+          placeholder={formControls.password.placeholder}
+          onChange={handleChange}
+          defaultValue={formControls.password.value}
+          touched={formControls.password.touched ? 1 : 0}
+          valid={formControls.password.valid}
+          required
+        />
+        {formControls.password.touched && !formControls.password.valid && (
+          <div className={"error-message"}>
+            {" "}
+            * Password must have minimum 5 characters
+          </div>
+        )}
+      </FormGroup>
+
+      <Row>
+        <Col sm={{ size: "4", offset: 4 }}>
+          <Button
+            type={"submit"}
+            disabled={!formIsValid}
+            onClick={handleSubmit}
+          >
+            {" "}
+            Login{" "}
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ "padding-top": "20px" }}>
+        <Col sm={{ size: "10", offset: 1 }}>
+          <strong> Not registered yet, Register now</strong>
+        </Col>
+      </Row>
+      <Row style={{ "padding-top": "20px" }}>
+        <Col sm={{ size: "4", offset: 4 }}>
+          <Button
+            type={"submit"}
+            disabled={!formIsValid}
+            onClick={handleSubmit}
+          >
+            {" "}
+            Register{" "}
+          </Button>
+        </Col>
+      </Row>
+
+      {error.status > 0 && (
+        <APIResponseErrorMessage
+          errorStatus={error.status}
+          error={error.errorMessage}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LoginForm;
--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import APIResponseErrorMessage from "../commons/errorhandling/api-response-error-message";
 import "../commons/styles/BackgroundStyle.css";
 import CarouselAnimation from "./CarouselAnimation";
+import LoginForm from "./LoginForm.js";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function LoginPage(props) {
   // Store error status and message in the same object because we don't want
@@ -17,7 +20,19 @@ function LoginPage(props) {
     <div className="background" style={{ width: "100%" }}>
       <div class="container" style={{ "margin-top": "50px", width: "100%" }}>
         <div class="row" style={{ "padding-top": "100px" }}>
-          <div class="col-6" />
+          <div class="col-6">
+            <Card className="text-center">
+              <Card.Header>Login</Card.Header>
+              <Card.Body>
+                <LoginForm />
+              </Card.Body>
+              
+              <Card.Footer className="text-muted">
+                {" "}
+                © all rights reserved to Birlutiu Claudiu-Andrei
+              </Card.Footer>
+            </Card>
+          </div>
           <div class="col-6" style={{ "tetx-align": "center" }}>
             <CarouselAnimation style={{ "padding-left": "100px" }} />
           </div>
