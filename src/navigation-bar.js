f423fa47baa0130353216a4a81b9635ae2248c83
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-10-25 01:56:02 +0300

    navbar customized for user

--- src/navigation-bar.js
+++ src/navigation-bar.js
@@ -24,8 +24,12 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/person">User Management</Nav.Link>
-            <Nav.Link href="/metering-devices">Metering devices</Nav.Link>
+            {user !== null && user.role === "ADMIN" && (
+              <Nav.Link href="/person">User Management</Nav.Link>
+            )}
+            {user !== null && user.role === "ADMIN" && (
+              <Nav.Link href="/metering-devices">Metering devices</Nav.Link>
+            )}
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
