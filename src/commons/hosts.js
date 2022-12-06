9f328b9f1bbbf7208d556d953f201e3300fad6e5
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-12-06 08:37:48 +0200

    added new port

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
-  backend_api: "http://20.113.55.142:8080/api",
-  webSocketApi: "http://20.113.55.142:8080/api/ws-message/",
+  backend_api: "http://20.113.7.53:8080/api",
+  webSocketApi: "http://20.113.7.53:8080/api/ws-message/",
 };
